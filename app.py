b3edf0e8854ed3901624f6bb446223025c0b2f0a
Kiran-coplot <pradeepkiran4@gmail.com>
2025-03-18 20:21:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Hello, Jenkins!")
+pipeline {
+    agent any
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Kiran-coplot/python-jenkins.git'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                sh 'pip install -r requirements.txt || echo "No dependencies to install"'
+            }
+        }
+        stage('Run Python Script') {
+            steps {
+                sh 'python3 app.py'
+            }
+        }
+    }
+}
+
